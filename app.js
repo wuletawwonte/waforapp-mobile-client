51c487c259bf431032c47f44d223011ed4180efd
Wuletaw <wuletawwonte@yahooo.com>
2019-06-29 10:04:54 +0300

    commit5

--- app.js
+++ app.js
@@ -78,26 +78,7 @@ app.controller('homeCtrl', function($scope, $location) {
 
 
 
-<?xml version="1.0" encoding="UTF-8" ?>
-<widget xmlns   = "http://www.w3.org/ns/widgets"
-    xmlns:gap   = "http://phonegap.com/ns/1.0"
-    id          = "com.phonegap.example"
-    versionCode = "10"
-    version     = "1.0.0" >
 
-<!-- versionCode is optional and Android only -->
-
-  <name>Waforapp</name>
-
-  <description>
-      An example for phonegap build docs.
-  </description>
-
-  <author href="https://build.phonegap.com" email="support@phonegap.com">
-      Wuletaw Wonte
-  </author>
-
-</widget>
 
 
 
